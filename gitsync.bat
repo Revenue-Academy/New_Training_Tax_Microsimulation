4dca941f87fc3e39d2c8f92e4018303c9e7717d0
Sebastian James <sebastiansajie@yahoo.com>
2022-09-30 04:15:49 -0400

    Update gitsync.bat

--- gitsync.bat
+++ gitsync.bat
@@ -4,12 +4,12 @@ SETLOCAL
 git branch
 
 :PROMPT
-SET /P ONMASTER=Are you on the master branch (y/n)?
+SET /P ONMASTER=Are you on the main branch (y/n)?
 IF /I "%ONMASTER%" NEQ "y" GOTO END
 
 git fetch upstream
-git merge upstream/master
-git push origin master
+git merge upstream/main
+git push origin main
 
 :END
 ENDLOCAL
